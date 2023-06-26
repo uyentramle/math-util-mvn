b13a6f4994ad48c317cb24ba4311d598cf799e68
uyentramle <uyentramle1010@gmail.com>
2023-06-26 09:07:39 +0700

    update getf() to use recursion

--- src/main/java/com/utl/mathutlil/core/MathUtility.java
+++ src/main/java/com/utl/mathutlil/core/MathUtility.java
@@ -18,9 +18,26 @@ public class MathUtility {
     // Khong co giai thua cho so am
     // 21! la kieu long khong chua noi, 20! = 18 con so 0 vua kip du cho kieu long Java
     // gai rang buoc dau vao cho ham / method 
+    
+//    public static long getFactorial(int n) {
+//        long product = 1; // tích ban đầu = 1
+//
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20, please.");
+//        }
+//
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//
+//        return product;
+//    }
+    // return som de ham khong bao loi
+    
     public static long getFactorial(int n) {
-        long product = 1; // tích ban đầu = 1
-
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n. n must be between 0..20, please.");
         }
@@ -28,11 +45,6 @@ public class MathUtility {
         if (n == 0 || n == 1) {
             return 1;
         }
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-
-        return product;
+        return n * getFactorial(n - 1);
     }
-    // return som de ham khong bao loi
 }
